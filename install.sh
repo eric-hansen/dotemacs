cd11cc23f490f5e186373e517b1c966ceb95deff
Eric Hansen <eric.hansen@ninjageekstudios.com>
2016-03-30 20:33:20 -0400

    Make this work finally

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 DATE=$(date +%m_%d_%y)
+PWD=$(pwd)
 
 if [ -e "$HOME/.emacs" ]; then
 	echo "$HOME/.emacs file exists, moving it to .emacs.$DATE"
 	mv "$HOME/.emacs" "$HOME/.emacs.$DATE"
 fi
 
-ln -s .emacs $HOME/.emacs
+ln -s "$PWD/.emacs" "$HOME/.emacs"
 
 if [ -e "$HOME/.emacs.d" ]; then
 	echo "$HOME/.emacs.d directory exists, moving it to .emacs.d.$DATE"
 	mv "$HOME/.emacs.d" "$HOME/.emacs.d.$DATE"
 fi
 
-ln -s .emacs.d $HOME/.emacs.d
+ln -s "$PWD/.emacs.d" "$HOME/.emacs.d"
