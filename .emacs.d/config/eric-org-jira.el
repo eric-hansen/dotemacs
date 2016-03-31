76f5101585ef2b322baf026eebed0b0724cca519
Eric Hansen <eric.hansen@ninjageekstudios.com>
2016-03-30 20:34:26 -0400

    Shows how to use eric-pkg

--- .emacs.d/config/eric-org-jira.el
+++ .emacs.d/config/eric-org-jira.el
@@ -1,14 +1,12 @@
 (setq jiralib-url "http://jira.nabsource.northamericanbancard.com")
 
-(require 'org-jira)
-;;  (org-jira-get-issues nil)
+(eric-pkg 'org-jira)
 
 (defun init-eric-org-jira ()
   "Enable ORG JIRA minor mode and fetch new issues"
   (interactive)
   (message "Sweet entry man")
   (unless org-jira-mode (org-jira-mode t))
-;;;  (org-jira-get-issues ())
   (call-interactively 'org-jira-get-issues)
 )
 
