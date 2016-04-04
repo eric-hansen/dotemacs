156706fb3be41978be415baf5b64d254024274e3
Eric Hansen <eric.hansen@ninjageekstudios.com>
2016-04-04 19:50:08 -0400

    This finally works, thanks to /r/emacs

--- .emacs.d/config/eric-weather.el
+++ .emacs.d/config/eric-weather.el
@@ -1,33 +1,24 @@
 (require 'request)
 (require 'json)
 
-(setq eric-weather-api nil)
+(defvar eric-weather-api nil)
 
 (defun eric-weather (zip)
   "Gets current weather for provided zip code"
   (interactive "sZip: ")
-  (if (not (boundp 'eric-weather-api))
-    (error "No API key provided.  Set eric-weather-api"))
-  (if (not (boundp 'eric-weather-unit))
-    (setq eric-weather-unit 'imperial))
-
-  (setq apiUrl 
-   (format "http://api.openweathermap.org/data/2.5/weather?zip=%s,us&units=%s&appid=%s" zip eric-weather-unit eric-weather-api)
-  )
-
-  (message "URL: %s" apiUrl)
-
-  (request
-    apiUrl
-   :parser 'json-read
-   :success (function*
-	   (lambda (&key data &allow-other-keys)
-	     (setq description (assoc-default 'description (elt (assoc-default 'weather data) 0)))
-	     (setq temp (assoc-default 'temp (assoc-default 'main data)))
-	     (setq city (assoc-default 'name data))
-	     (message "%s is %s @ %s degrees" city description temp)  
-	     )
-	   )
-  ))
+  (unless eric-weather-api
+    (user-error "No API key provided.  Set eric-weather-api"))
+  (let* ((apiUrl
+          (format "http://api.openweathermap.org/data/2.5/weather?zip=%s,us&units=imperial&appid=%s" zip eric-weather-api)
+	  ))
+    (request
+     apiUrl
+     :parser 'json-read
+     :success (function*
+               (lambda (&key data &allow-other-keys)
+                 (let ((description (assoc-default 'description (elt (assoc-default 'weather data) 0)))
+                       (temp (assoc-default 'temp (assoc-default 'main data)))
+                       (city (assoc-default 'name data)))
+                   (message "%s is %s @ %s degrees" city description temp)))))))
 
 (provide 'eric-weather)
