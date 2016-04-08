17bc56445a5a13344f4269b4716676e186ef1e85
Eric Hansen <echansen@nabancard.com>
2016-04-08 08:20:03 -0400

    Make this a bit more user-friendly.
    While this might be super-config-heavy, it allows better customization
    as not everyone wants to do things the way I do.  But, the defaults are
    for my use.

--- .emacs.d/config/eric-auth.el
+++ .emacs.d/config/eric-auth.el
@@ -2,10 +2,14 @@
 
 (epa-file-enable)
 
-(defvar eric-auth-extension ".el.gpg" "The extension of encrypted files")
+(defvar eric-auth-extension "el.gpg" "The extension of encrypted files")
+(defvar eric-auth-path "~" "The path where passwd file is stored")
+(defvar eric-auth-file "passwd" "The filename to load for passwords and other sensitive information")
+(defvar eric-auth nil "The full path + filename to load, only set this on init so the variables can be changed prior to setting up")
 
 (defun eric-auth-init ()
   (interactive)
-  (add-to-list 'load-suffixes eric-auth-extension))
+  (add-to-list 'load-suffixes eric-auth-extension)
+  (setq eric-auth (format "%s/%s.%s" eric-auth-path eric-auth-file eric-auth-extension)))
 
 (provide 'eric-auth)
