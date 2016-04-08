ed20b9c4002b89cace1f55ef08d86b7f7d1f5d00
Eric Hansen <echansen@nabancard.com>
2016-04-08 08:26:05 -0400

    Added more comments and a loader func.
    The loader function should be called instead of require if you want some
    sane error checking (i.e.: auth file not existing or unreadable).  While
    it's perfectly acceptable to not use this, I just figure it would be
    nice to drill down some Elisp basics for myself as well.

--- .emacs.d/config/eric-auth.el
+++ .emacs.d/config/eric-auth.el
@@ -7,9 +7,17 @@
 (defvar eric-auth-file "passwd" "The filename to load for passwords and other sensitive information")
 (defvar eric-auth nil "The full path + filename to load, only set this on init so the variables can be changed prior to setting up")
 
+;; Due to the use of configurable vars above, we really should force an init method in case they change.
+;; A way around this would be to do customize settings but that will be a WIP for me.
 (defun eric-auth-init ()
   (interactive)
   (add-to-list 'load-suffixes eric-auth-extension)
   (setq eric-auth (format "%s/%s.%s" eric-auth-path eric-auth-file eric-auth-extension)))
+
+;; Use this instead of simply (require <passwd>) for better error handling (ish?)
+(defun eric-auth-load ()
+  (unless (file-exists-p eric-auth)
+    (error "Auth file is either unreadable or does not exist: %s" eric-auth))
+  (require eric-auth-file))
 
 (provide 'eric-auth)
